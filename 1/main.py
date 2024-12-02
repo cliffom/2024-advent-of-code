0b682b151c17b0d251c003587fbc1287fa25e41a
Michael Clifford <cliffom@gmail.com>
2024-12-02 15:55:06 -0800

    day1 similarity_score in python

--- 1/main.py
+++ 1/main.py
@@ -8,6 +8,19 @@ def sum_columns(col1, col2):
         total += abs(num1 - num2)
     return total
 
+def get_similarity_score(column1, column2):
+    count_map = {}
+    for value in column2:
+        count_map[value] = count_map.get(value, 0) + 1
+
+    score = 0
+    for value in column1:
+        if value in count_map:
+            score += value * count_map[value]
+
+    return score
+
+
 def main():
     col1 = []
     col2 = []
@@ -25,7 +38,9 @@ def main():
     col2.sort()
 
     result = sum_columns(col1, col2)
+    similarity_score = get_similarity_score(col1, col2)
     print(result)
+    print(similarity_score)
 
 if __name__ == "__main__":
     main()

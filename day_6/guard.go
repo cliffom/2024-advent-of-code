c78c6bf8e650fd413a0316ebdd469c8b634ea9e4
Michael Clifford <cliffom@gmail.com>
2024-12-06 14:29:50 -0800

    Cleanups

--- day_6/guard.go
+++ day_6/guard.go
@@ -1,23 +1,33 @@
 package main
 
-var coordinates = [4][2]int{
-	{-1, 0}, // Up
-	{0, 1},  // Right
-	{1, 0},  // Down
-	{0, -1}, // Left
-}
-
-var guardFrames = [4]rune{
-	rune(int('^')), // Up
-	rune(int('>')), // Right
-	rune(int('v')), // Down
-	rune(int('<')), // Left
-}
+const guardMovementDirections = 4
 
 type Guard struct {
 	CurrentDirection int
 	CurrentPosition  [2]int
 	Map              AreaMap
+}
+
+func (g *Guard) MovementModifier() [2]int {
+	coordinates := [guardMovementDirections][2]int{
+		{-1, 0}, // Up
+		{0, 1},  // Right
+		{1, 0},  // Down
+		{0, -1}, // Left
+	}
+
+	return coordinates[g.CurrentDirection]
+}
+
+func (g *Guard) CurrentFrame() rune {
+	guardFrames := [guardMovementDirections]rune{
+		rune(int('^')), // Up
+		rune(int('>')), // Right
+		rune(int('v')), // Down
+		rune(int('<')), // Left
+	}
+
+	return guardFrames[g.CurrentDirection]
 }
 
 func (g *Guard) GetCurrentPosition() [2]int {
@@ -26,22 +36,21 @@ func (g *Guard) GetCurrentPosition() [2]int {
 
 func (g *Guard) GetNextPosition() [2]int {
 	position := g.GetCurrentPosition()
-	x := position[0] + coordinates[g.CurrentDirection][0]
-	y := position[1] + coordinates[g.CurrentDirection][1]
+	nextPosition := [2]int{position[0] + g.MovementModifier()[0], position[1] + g.MovementModifier()[1]}
 
-	return [2]int{x, y}
+	return nextPosition
 }
 
 func (g *Guard) SetPosition(position [2]int) {
 	x := position[0]
 	y := position[1]
 	g.CurrentPosition = [2]int{x, y}
-	g.Map.SetContentsAtPosition(g.CurrentPosition, guardFrames[g.CurrentDirection])
+	g.Map.SetContentsAtPosition(g.CurrentPosition, g.CurrentFrame())
 }
 
 func (g *Guard) ChangeDirection() {
 	g.CurrentDirection += 1
-	if g.CurrentDirection >= len(coordinates) {
+	if g.CurrentDirection >= guardMovementDirections {
 		g.CurrentDirection = 0
 	}
 }
